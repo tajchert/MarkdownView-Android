7777d039f21e0588ec357c05ed3b309e8a28b1b1
Michal Tajchert <thetajchert@gmail.com>
2016-10-26 00:45:08 +0200

    Example with runnable which fails without invalidate.

--- app/src/main/java/com/mukesh/markdownview/example/MainActivity.java
+++ app/src/main/java/com/mukesh/markdownview/example/MainActivity.java
@@ -1,16 +1,27 @@
 package com.mukesh.markdownview.example;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import com.mukesh.MarkdownView;
 
 public class MainActivity extends AppCompatActivity {
+  MarkdownView markdownView;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    MarkdownView markdownView = (MarkdownView) findViewById(R.id.markdown_view);
-    markdownView.setMarkDownText("# Hello World\nThis is a simple markdown");
+    markdownView = (MarkdownView) findViewById(R.id.markdown_view);
     //markdownView.loadMarkdownFromAssets("README.md");
+    Handler handler = new Handler();
+    final Runnable r = new Runnable() {
+      public void run() {
+        if(markdownView != null) {
+          markdownView.setMarkDownText("# Hello World\nThis is a simple markdown");
+        }
+      }
+    };
+
+    handler.postDelayed(r, 1000);
   }
 }
